1461f15b91d35675a275cc264477a57c00306326
whydda <whydda@naver.com>
2020-05-05 06:54:22 +0900

    Initial commit

---
+++ src/main/java/com/example/gitflowtest/GitflowtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitflowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitflowtest/GitflowtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
